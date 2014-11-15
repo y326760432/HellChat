5359c67590d93abac7afe196d6bcbc392b3869be
叶根长 <326760432@qq.com>
2014-11-15 08:48:40 +0800

    动态模块

---
+++ HelloChat/HCDynamicController.h
@@ -0,0 +1,13 @@
+//
+//  HCDynamicController.h
+//  HelloChat
+//
+//  Created by 叶根长 on 14-11-2.
+//  Copyright (c) 2014年 叶根长. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HCDynamicController : UIViewController
+
+@end
---
+++ HelloChat/HCDynamicController.m
@@ -0,0 +1,25 @@
+//
+//  HCDynamicController.m
+//  HelloChat
+//
+//  Created by 叶根长 on 14-11-2.
+//  Copyright (c) 2014年 叶根长. All rights reserved.
+//
+
+#import "HCDynamicController.h"
+
+@interface HCDynamicController ()
+
+@end
+
+@implementation HCDynamicController
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.title=@"动态";
+}
+
+
+@end
