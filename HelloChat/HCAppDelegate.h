9c8f033a6ebd712ebbb036a0b9180dcd2d6ce8ed
叶根长 <326760432@qq.com>
2014-11-15 14:41:36 +0800

    导入XMPP框架

--- HelloChat/HCAppDelegate.h
+++ HelloChat/HCAppDelegate.h
@@ -7,9 +7,54 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "XMPPFramework.h"
+#import "XMPPvCardTempModule.h"
+#import "XMPPRoster.h"
+/**
+ 连接失败调用的Block
+ */
+typedef void (^connectFailBlock)(NSString *error);
 
-@interface HCAppDelegate : UIResponder <UIApplicationDelegate>
+/**
+ 连接成功调用的Block
+ */
+typedef void (^connectSuccessBlock)();
+
+@interface HCAppDelegate : UIResponder <UIApplicationDelegate,XMPPStreamDelegate>
+
+/**
+ xmpp
+ */
+@property(nonatomic,readonly) XMPPStream *xmppStream;
+
+/**
+电子名片
+ */
+@property(nonatomic,readonly) XMPPvCardTempModule *xmppvCardTempModule;
+
+/**
+ *  全局的XMPPvCardAvatar模块，处理头像
+ */
+@property (strong, nonatomic, readonly) XMPPvCardAvatarModule *xmppvCardAvatarModule;
+
+/**
+ 花名片
+ */
+@property(strong,nonatomic,readonly) XMPPRoster *xmppRoster;
+
+@property(nonatomic,strong,readonly) XMPPRosterCoreDataStorage *xmpprosterCoreDataStorage;
+
+/**
+ 连接
+ */
+-(void)connectWithFailBock:(connectFailBlock) failblock succsee:(connectSuccessBlock)successblock;
+
+/**
+ 断开连接
+ */
+-(void)disconnect;
 
 @property (strong, nonatomic) UIWindow *window;
+
 
 @end
--- HelloChat/HCAppDelegate.m
+++ HelloChat/HCAppDelegate.m
@@ -7,43 +7,332 @@
 //
 
 #import "HCAppDelegate.h"
+#import "HCLoginController.h"
+#import "HCMainController.h"
+#import "RESideMenu.h"
+#import "HCLeftMenuController.h"
+#import "HCLoginUser.h"
+#import "HCLoginUserTool.h"
+#import "XMPPReconnect.h"
+#import "XMPPvCardCoreDataStorage.h"
+#import "XMPPvCardTempModule.h"
+@interface HCAppDelegate ()<XMPPvCardTempModuleDelegate,XMPPRosterDelegate>
+{
+    connectFailBlock _connectFialBlock;//连接失败调用的Block 包括连接，验证密码错误，都调用整个block
+    connectSuccessBlock _connectSuccessBlock;//连接成功调用的Block
+    
+    XMPPReconnect *_xmppreconect;//网络断开后自动重新连接
+    
+    XMPPvCardCoreDataStorage *_vcardCoreDataStorage;//电子名片存储模块
+    
+    HCMainController *_mainController;//主控制器
+    
+    HCLoginController *_loginController;//登录控制器
+    
+    XMPPCapabilities *_xmppcapabilities;//增强实体连接模块
+    XMPPCapabilitiesCoreDataStorage *_xmppcapabilitiesStorage;//增强实体连接模块
+}
+@end
 
 @implementation HCAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+    
+    
+    //如果沙盒中有用户登录信息，则直接到主控制器，否则启动登录控制器
+    if([HCLoginUserTool sharedHCLoginUserTool].loginUser&&[HCLoginUserTool sharedHCLoginUserTool])
+        [self goMainController];
+    else
+        [self goLoginController];
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
 }
 
+#pragma mark  前往登录控制器
+-(void)goLoginController
+{
+    if(_loginController==nil)
+        _loginController=[[HCLoginController alloc]init];
+     self.window.rootViewController=_loginController;
+}
+
+#pragma mark 前往主控制器(登录成功)
+-(void)goMainController
+{
+    if(_mainController==nil)
+    {
+        //初始化主控制并且设置左边侧滑视图
+//        _mainController=[[RESideMenu alloc]initWithContentViewController:[[HCMainController alloc]init] leftMenuViewController:[[HCLeftMenuController alloc]init] rightMenuViewController:nil];
+//        //设置侧滑视图的背景图片
+//        _mainController.backgroundImage=[UIImage imageNamed:@"Stars.png"];
+        
+    }
+    _mainController=[[HCMainController alloc]init];
+    self.window.rootViewController=_mainController;
+}
+
+#pragma mark XMPP相关
+-(void)setUpXmppStream
+{
+    if(_xmppStream==nil)
+    {
+        _xmppStream=[[XMPPStream alloc]init];
+        
+        // 让XMPP在真机运行时支持后台，在模拟器上是不支持后台服务运行的
+#if !TARGET_IPHONE_SIMULATOR
+        {
+            // 允许XMPPStream在真机运行时，支持后台网络通讯！
+            [_xmppStream setEnableBackgroundingOnSocket:YES];
+        }
+#endif
+        
+        //注册重连接模块
+        _xmppreconect=[[XMPPReconnect alloc]init];
+        
+        //设置代理
+        [_xmppStream addDelegate:self delegateQueue:dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)];
+        
+        //电子名片模块
+        _vcardCoreDataStorage=[XMPPvCardCoreDataStorage sharedInstance];
+        _xmppvCardTempModule=[[XMPPvCardTempModule alloc]initWithvCardStorage:_vcardCoreDataStorage];
+        _xmppvCardAvatarModule=[[XMPPvCardAvatarModule alloc] initWithvCardTempModule:_xmppvCardTempModule];
+        //设置代理
+        [_xmppvCardTempModule addDelegate:self delegateQueue:dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)];
+        
+        //花名册
+        _xmpprosterCoreDataStorage=[[XMPPRosterCoreDataStorage alloc]init];
+        _xmppRoster=[[XMPPRoster alloc]initWithRosterStorage:_xmpprosterCoreDataStorage];
+        //自动接收好友请阅请求 双向关注好友
+        _xmppRoster.autoAcceptKnownPresenceSubscriptionRequests=YES;
+        //自动更新好友信息（好友信息发送改变后会通知客户端）
+        _xmppRoster.autoFetchRoster=YES;
+        [_xmppRoster addDelegate:self delegateQueue:dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)];
+        
+        //实体扩展模块
+        _xmppcapabilitiesStorage =[[XMPPCapabilitiesCoreDataStorage alloc]init];
+        _xmppcapabilities=[[XMPPCapabilities alloc]initWithCapabilitiesStorage:_xmppcapabilitiesStorage];
+        
+        //激活
+        [_xmppreconect activate:_xmppStream];
+        [_xmppvCardTempModule activate:_xmppStream];
+        [_xmppvCardAvatarModule activate:_xmppStream];
+        [_xmppRoster activate:_xmppStream];
+        [_xmppcapabilities activate:_xmppStream];
+    }
+
+}
+
+#pragma mark 开始连接服务器
+-(void)connectWithFailBock:(connectFailBlock)failblock succsee:(connectSuccessBlock)successblock
+{
+    _connectFialBlock=failblock;
+    _connectSuccessBlock=successblock;
+    [self setUpXmppStream];
+    [_xmppStream disconnect];
+    
+    if (![_xmppStream isConnected]) {
+        XMPPJID *jid = [XMPPJID jidWithString:[HCLoginUserTool sharedHCLoginUserTool].loginUser.JID];
+        [self.xmppStream setMyJID:jid];
+        [self.xmppStream setHostName:kHostName];
+        NSError *error = nil;
+        [_xmppStream connectWithTimeout:1 error:&error];
+        if(error&&_connectFialBlock)
+        {
+            dispatch_async(dispatch_get_main_queue(), ^{
+            _connectFialBlock(@"连接服务器出错,请检查网络是否启用");
+            });
+        }
+    }
+    
+}
+
+#pragma mark 验证密码
+-(void)xmppStreamDidConnect:(XMPPStream *)sender
+{
+    NSError *error = nil;
+    if (![self.xmppStream authenticateWithPassword:[HCLoginUserTool sharedHCLoginUserTool].loginUser.password error:&error]) {
+        if(error&&_connectFialBlock)
+        {
+            dispatch_async(dispatch_get_main_queue(), ^{
+            _connectFialBlock(@"用户名或密码错误,请重新输入!");
+            });
+        }
+    }
+}
+
+#pragma mark 连接失败
+-(void)xmppStreamDidDisconnect:(XMPPStream *)sender withError:(NSError *)error
+{
+    if(_connectFialBlock&&error)
+    {
+        dispatch_async(dispatch_get_main_queue(), ^{
+        _connectFialBlock(@"连接失败!");
+        });
+    }
+}
+
+#pragma mark 密码验证失败
+-(void)xmppStream:(XMPPStream *)sender didNotAuthenticate:(DDXMLElement *)error
+{
+    if(error&&_connectFialBlock)
+    {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            _connectFialBlock(@"用户名或密码错误!");
+        });
+    }
+}
+
+#pragma mark 验证完毕
+-(void)xmppStreamDidAuthenticate:(XMPPStream *)sender
+{
+    BOOL islogined=[HCLoginUserTool sharedHCLoginUserTool].isLogined;
+    if(!islogined)
+    {
+        [self goMainController];
+    }
+    //登录成功回调
+    if(_connectSuccessBlock)
+    {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            _connectSuccessBlock();
+        });
+    }
+    [HCLoginUserTool sharedHCLoginUserTool].isLogined=YES;
+    [self goOnline];
+}
+
+#pragma mark 上线
+-(void)goOnline
+{
+    NSLog(@"上线");
+    XMPPPresence *presence = [XMPPPresence presenceWithType:@"available"];
+    [self.xmppStream sendElement:presence];
+    
+}
+
+-(void)xmppvCardTempModule:(XMPPvCardTempModule *)vCardTempModule didReceivevCardTemp:(XMPPvCardTemp *)vCardTemp forJID:(XMPPJID *)jid
+{
+     [[NSNotificationCenter defaultCenter] postNotificationName:kdidupdatevCard object:nil];
+}
+
+#pragma mark 电子名片保存成功
+-(void)xmppvCardTempModuleDidUpdateMyvCard:(XMPPvCardTempModule *)vCardTempModule
+{
+    //发送通知
+    [[NSNotificationCenter defaultCenter] postNotificationName:kdidupdatevCard object:nil];
+}
+
+#pragma mark 电子名片保存失败
+-(void)xmppvCardTempModule:(XMPPvCardTempModule *)vCardTempModule failedToUpdateMyvCard:(DDXMLElement *)error
+{
+    //发送通知
+    [[NSNotificationCenter defaultCenter] postNotificationName:kupdatevCardFaild object:nil];
+}
+
+#pragma  mark 接受到消息
+-(void)xmppStream:(XMPPStream *)sender didReceiveMessage:(XMPPMessage *)message
+{
+    NSLog(@"%@",message);
+}
+
+#pragma mark 接收到用户展现(好友请求，上线，下线等)
+
+-(void)xmppStream:(XMPPStream *)sender didReceivePresence:(XMPPPresence *)presence
+{
+    //如果是好友请求的展现
+    if([presence.type isEqualToString:@"subscribe"])
+    {
+        XMPPJID *jid=presence.from;
+        //接收请求
+        [_xmppRoster acceptPresenceSubscriptionRequestFrom:jid andAddToRoster:YES];
+    }
+    NSLog(@"didReceivePresence---%@",presence);
+}
+//
+//-(void)xmppRoster:(XMPPRoster *)sender didReceivePresenceSubscriptionRequest:(XMPPPresence *)presence
+//{
+//    NSLog(@"didReceivePresenceSubscriptionRequest---%@",presence);
+//}
+
+#pragma mark 下线
+-(void)goOffline
+{
+    XMPPPresence *presence = [XMPPPresence presenceWithType:@"unavailable"];
+    [self.xmppStream sendElement:presence];
+}
+
+#pragma mark 断开连接
+- (void)disconnect
+{
+    [self goOffline];
+    [self.xmppStream disconnect];
+}
+
+// 销毁XMPPStream并注销已注册的扩展模块
+- (void)teardownStream
+{
+    // 1. 删除代理
+    [_xmppStream removeDelegate:self];
+    // 2. 取消激活在setupStream方法中激活的扩展模块
+    [_xmppreconect deactivate];
+    [_xmppvCardTempModule deactivate];
+    [_xmppvCardAvatarModule deactivate];
+    [_xmppRoster deactivate];
+    [_xmppcapabilities deactivate];
+    
+    // 3. 断开XMPPStream的连接
+    [_xmppStream disconnect];
+    
+    // 4. 内存清理
+    _xmppStream = nil;
+    _xmppreconect = nil;
+    _xmppvCardTempModule = nil;
+    _xmppvCardAvatarModule = nil;
+    _vcardCoreDataStorage = nil;
+    _xmppRoster=nil;
+    _xmpprosterCoreDataStorage=nil;
+    _xmppcapabilities=nil;
+    _xmppcapabilitiesStorage=nil;
+}
+
+#pragma mark 当前窗口失去焦点，即将进入后台
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    
 }
 
+#pragma mark 程序进入后台
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+   
 }
 
+#pragma mark 即将进入前台
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    
 }
 
+#pragma mark 重新获取焦点
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    
+}
+
+//实例销毁
+-(void)dealloc
+{
+    //销毁XMPP相关
+    [self teardownStream];
 }
 
 @end
